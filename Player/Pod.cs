c2e581f615e101f369997b13162a27a4011538cb
SYLVAINCOUSDC6F\Sylvain <sylvain.cousquer@gmail.com>
2017-03-03 18:12:17 +0100

    Add pod class

---
+++ Player/Pod.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrigoUtilities;
+
+namespace Player
+{
+    public class Pod
+    {
+        public int Angle { get; private set; }
+        public EuclidianVector Speed { get; private set; }
+        public Point Position;
+       
+
+        public Pod(int x, int y, int vx, int vy, int angle)
+        {
+            Position = new Point(x,y);
+            Speed = new EuclidianVector(vx,vy);
+           
+            Angle = angle;
+        }
+    }
+}
---
+++ PodTests/PodTest.cs
@@ -0,0 +1,28 @@
+﻿
+using NUnit.Framework;
+using Player;
+
+namespace PodTests
+{
+    [TestFixture]
+    public class PodTest
+    {
+        [Test]
+        public void CtorShouldCreateaPod()
+        {
+            const int x = 100;
+            const int y = 152;
+            const int vx = -745;
+            const int vy = 542;
+            const int angle = 75;
+            var pod = new Player.Pod( x,  y,  vx,  vy, angle);
+
+            Assert.That(pod.Position.X,Is.EqualTo(x));
+            Assert.That(pod.Position.Y,Is.EqualTo(y));
+            Assert.That(pod.Speed.X,Is.EqualTo(vx));
+            Assert.That(pod.Speed.Y,Is.EqualTo(vy));
+            Assert.That(pod.Angle,Is.EqualTo(angle));
+           
+        }
+    }
+}
